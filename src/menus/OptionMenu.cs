54b9ec90fb8192df6fb74a28a1a8bd72bf809e25
Louis Gallet <me@louisgallet.fr>
2024-02-05 14:09:23 +0100

    Feature/gui implementation (#25)
    * feat: :construction:  Starting implementing the main menu
    
    * feat: :lipstick:  Create new UI for the game
    
    * feat: :lipstick:  Add responsive to different format screen
    
    * feat: :fire:  Remove csproj.old
    
    * chore: :see_no_evil:  Add old file to gitignore
    
    * chore: :fire:  Remove old csproj
    
    * chore: :see_no_evil:  Add just old to gitignore

---
+++ src/menus/OptionMenu.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public partial class OptionMenu : CanvasLayer
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	/// <summary>
+	/// Called when the keymap button is pressed
+	/// </summary>
+	private void _on_keymap_button_button_down()
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// Called when the volumes button is pressed
+	/// </summary>
+	private void _on_volumes_button_button_down()
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// Called when the exit button is pressed
+	/// </summary>
+	private void _on_exit_button_button_down()
+	{
+		GetTree().ChangeSceneToFile("res://src/menus/MainMenu.tscn");
+	}
+ }
