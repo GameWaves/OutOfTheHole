27fdeb90b1c569bf5985e870ac7d9d027c4cfa76
bastien <bastien.pinaud@epita.fr>
2024-02-19 19:40:17 +0100

    refactor(Players): :recycle:  Delete duplicated code

--- src/Multiplayer/SceneManager.cs
+++ src/Multiplayer/SceneManager.cs
@@ -16,29 +16,33 @@ public partial class SceneManager : Node2D
 	{
 		var index = 0;
 		foreach (var item in GameManager.Players)
+		{
+			Player currentPlayer;
 			if (item.Role == 1)
 			{
-				var currentPlayer = _player1Scene.Instantiate<Player>();
+				currentPlayer = _player1Scene.Instantiate<Player>();
 				currentPlayer.Name = item.Id.ToString();
 				currentPlayer.Reversed = false;
-				AddChild(currentPlayer);
-				foreach (Node2D spawnPoint in GetTree().GetNodesInGroup("SpawnPoints"))
-					if (int.Parse(spawnPoint.Name) == index)
-						currentPlayer.GlobalPosition = spawnPoint.GlobalPosition;
-				index++;
 			}
 			else if (item.Role == 2)
 			{
-				var currentPlayer = _player2Scene.Instantiate<Player>();
-				currentPlayer.Name = item.Id.ToString();
+				currentPlayer = _player2Scene.Instantiate<Player>();
 				currentPlayer.Gravity = -currentPlayer.Gravity;
 				currentPlayer.Reversed = true;
-				AddChild(currentPlayer);
-				foreach (Node2D spawnPoint in GetTree().GetNodesInGroup("SpawnPoints"))
-					if (int.Parse(spawnPoint.Name) == index)
-						currentPlayer.GlobalPosition = spawnPoint.GlobalPosition;
-				index++;
 			}
+			else
+			{
+				throw new ArgumentException("Role should be 1 or 2");
+			}
+			
+			currentPlayer.Name = item.Id.ToString();
+			AddChild(currentPlayer);
+			foreach (Node2D spawnPoint in GetTree().GetNodesInGroup("SpawnPoints"))
+				if (int.Parse(spawnPoint.Name) == index)
+					currentPlayer.GlobalPosition = spawnPoint.GlobalPosition;
+			index++;
+		}
+			
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
