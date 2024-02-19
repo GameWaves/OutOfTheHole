2a291841e69eb4859fcbf40afa0942ae2dc02f30
bastien <bastien.pinaud@epita.fr>
2024-02-19 20:06:02 +0100

    refactor(GameOver): :recycle:  refactor + packedscene

--- src/Gameover/GameOver.cs
+++ src/Gameover/GameOver.cs
@@ -4,17 +4,20 @@ namespace OutofTheHole.Gameover;
 
 public partial class GameOver : Node2D
 {
+	[Export] private PackedScene _fallbackscene;
+	
 	/// <summary>
-	///     function that is called when a button is pressed (currently the retry button)
+	/// function that is called when a button is pressed (currently the retry button)
 	/// </summary>
 	private void _on_button_pressed()
     {
-        // change the scene to the first scene
-        GetTree().ChangeSceneToFile("res://Map.tscn");
+        // change the scene to the selected fallback scene
+        GetTree().ChangeSceneToPacked(_fallbackscene);
+        //GetTree().ChangeSceneToFile("res://Map.tscn");
     }
 
 	/// <summary>
-	///     function that is called when a button is pressed (currently the leave button)
+	/// function that is called when a button is pressed (currently the leave button)
 	/// </summary>
 	private void _on_button_2_pressed()
     {
